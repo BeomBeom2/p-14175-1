31a41e1c4bb146fb33dbc6f82e311f4ced1d5b34
BeomBeom2 <whwjdqja20@naver.com>
2026-05-29 10:22:50 +0900

    15강

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function Page() {
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+      .then((res) => res.json())
+      .then(setPosts);
+  }, []);
+
+  return (
+    <>
+      <h1>글 목록</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
