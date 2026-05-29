b9227feb0c3a28faf6f92a9847fcf0f60d5acee0
BeomBeom2 <whwjdqja20@naver.com>
2026-05-29 11:10:49 +0900

    19강

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -25,9 +25,9 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
 
       {post != null && (
         <>
-          <div>번호 : {post?.id}</div>
-          <div>제목: {post?.title}</div>
-          <div style={{ whiteSpace: "pre-line" }}>{post?.content}</div>
+          <div>번호 : {post.id}</div>
+          <div>제목: {post.title}</div>
+          <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
         </>
       )}
     </>
